e79832c79af5503110382d58f67e1ce218d7be2c
Marco Ruiz <martulioruiz@gmail.com>
2018-03-08 13:58:47 -0600

    031- Creando las rutas de recurso para cada Controllador
    1-Se crearon rutas de tipo recurso con excepciones para los metodos =[create y edit] para los recursos =[user y category]
    
    2-los demas controladores incluian solo los metodos =[index y show]
    recursos = [buyer, seller, product, transaction]
    
    3-
    +--------+-----------+---------------------------+-------------------+--------------------------------------------------------------+------------+
    | Domain | Method    | URI                       | Name              | Action                                                       | Middleware |
    +--------+-----------+---------------------------+-------------------+--------------------------------------------------------------+------------+
    |        | GET|HEAD  | /                         |                   | Closure                                                      | web        |
    |        | GET|HEAD  | buyer                     | buyer.index       | App\Http\Controllers\buyer\buyercontroller@index             | api        |
    |        | GET|HEAD  | buyer/{buyer}             | buyer.show        | App\Http\Controllers\buyer\buyercontroller@show              | api        |
    |        | GET|HEAD  | category                  | category.index    | App\Http\Controllers\category\categorycontroller@index       | api        |
    |        | POST      | category                  | category.store    | App\Http\Controllers\category\categorycontroller@store       | api        |
    |        | GET|HEAD  | category/{category}       | category.show     | App\Http\Controllers\category\categorycontroller@show        | api        |
    |        | PUT|PATCH | category/{category}       | category.update   | App\Http\Controllers\category\categorycontroller@update      | api        |
    |        | DELETE    | category/{category}       | category.destroy  | App\Http\Controllers\category\categorycontroller@destroy     | api        |
    |        | GET|HEAD  | product                   | product.index     | App\Http\Controllers\product\productcontroller@index         | api        |
    |        | GET|HEAD  | product/{product}         | product.show      | App\Http\Controllers\product\productcontroller@show          | api        |
    |        | GET|HEAD  | seller                    | seller.index      | App\Http\Controllers\seller\sellercontroller@index           | api        |
    |        | GET|HEAD  | seller/{seller}           | seller.show       | App\Http\Controllers\seller\sellercontroller@show            | api        |
    |        | GET|HEAD  | transaction               | transaction.index | App\Http\Controllers\transaction\transactioncontroller@index | api        |
    |        | GET|HEAD  | transaction/{transaction} | transaction.show  | App\Http\Controllers\transaction\transactioncontroller@show  | api        |
    |        | GET|HEAD  | user                      | user.index        | App\Http\Controllers\user\usercontroller@index               | api        |
    |        | POST      | user                      | user.store        | App\Http\Controllers\user\usercontroller@store               | api        |
    |        | GET|HEAD  | user/{user}               | user.show         | App\Http\Controllers\user\usercontroller@show                | api        |
    |        | PUT|PATCH | user/{user}               | user.update       | App\Http\Controllers\user\usercontroller@update              | api        |
    |        | DELETE    | user/{user}               | user.destroy      | App\Http\Controllers\user\usercontroller@destroy             | api        |
    +--------+-----------+---------------------------+-------------------+--------------------------------------------------------------+------------+

--- routes/api.php
+++ routes/api.php
@@ -12,5 +12,15 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::resource('buyer','buyer\buyercontroller',['only' => ['index','show']]);
 
+Route::resource('category','category\categorycontroller',['except' => ['create','edit']]);
+
+Route::resource('product','product\productcontroller',['only' => ['index','show']]);
+
+Route::resource('transaction','transaction\transactioncontroller',['only' => ['index','show']]);
+
+Route::resource('seller','seller\sellercontroller',['only' => ['index','show']]);
+
+Route::resource('user','user\usercontroller',['except' => ['create','edit']]);
 
