578eb69017c8b8b66d333e63ea8fa5dd7a1bd257
Marco Ruiz <martulioruiz@gmail.com>
2018-03-09 13:45:19 -0600

    035-Implementando las propiedades para User
    1-se crearon los atributos masivos = [name, password, email, verified, verification_token, admin]
    
    2-se agrego a verification_token a los atributos hidden o protegidos
    
    3-se crearon constantes para el atributo verified = [USUARIO_VERIFICADO, USUARIO_NO_VERIFICADO]
    
    4-se crearon constantes para el atributo admin = [USUARIO_REGULAR, USUARIO_Administrador]
    
    5-se crearon 2 funciones:
    una para saber si el usuario es administrador y otra para saber si esta verificado
    
    6-se creo una funcion estatica para generar el token de verificacion con la funcion str_random()

--- app/User.php
+++ app/User.php
@@ -10,13 +10,24 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    const USUARIO_VERIFICADO = '1';
+    const USUARIO_NO_VERIFICADO = '0';
+
+    const USUARIO_REGULAR = 'false';
+    const USUARIO_ADMINISTRADOR ='true'
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'verified',
+        'verification_token',
+        'admin',
     ];
 
     /**
@@ -25,6 +36,18 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
+        'verification_token',
     ];
+
+    public function esVerificado(){
+        return $this->verified == User::USUARIO_VERIFICADO;
+    }
+    public function esAdministrador(){
+        return $this->admin == User::USUARIO_ADMINISTRADOR;
+    }
+    public static function generarVerificationToken(){
+        return str_random(40);
+    }
 }
