e571dc9a618a969523102fd74ba241612d81b4b0
Marco Ruiz <martulioruiz@gmail.com>
2018-03-09 13:20:31 -0600

    033-Implementando las propiedades para product
    1-se crearon las propiedades masivas =[name,descripcion,quantity,status,seller_id]
    
    2-se declararon 2 constantes = [PRODUCTO_DISPONIBLE, PRODUCTO_NO_DISPONIBLE]
    
    3-se creo una funcion para saber si el producto esta disponible segun el status.

--- app/product.php
+++ app/product.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class product extends Model
 {
-    //
+	const PRODUCTO_DISPONIBLE = 'disponible';
+	const PRODUCTO_NO_DISPONIBLE = 'no disponible';
+
+    protected $fillable = [
+    	'name',
+    	'description',
+    	'quantity',
+    	'status',
+    	'image',
+    	'seller_id',
+    ];
+
+    public function estaDisponible(){
+    	return $this->status == product::PRODUCTO_DISPONIBLE;
+    }
 }
