e1bf2950d6bc77b72c5813213d6f19c2389879c4
Marco Ruiz <martulioruiz@gmail.com>
2018-03-16 12:20:41 -0600

    041-Implementando la migracion para Categories
    1-Se agregaron campos a la migracion del modelo Category =[name,description]
    
    2-se corrigieron detalles

--- database/migrations/2018_03_08_191909_create_products_table.php
+++ database/migrations/2018_03_08_191909_create_products_table.php
@@ -17,7 +17,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('description');
+            $table->string('description',1000);
             $table->string('quantity')->unsigned();
             $table->string('status')->default(product::PRODUCTO_NO_DISPONIBLE);
             $table->integer('seller_id')->unsigned();
